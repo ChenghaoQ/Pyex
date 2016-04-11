992d80671a00995a4701970633ac7298fdc7d040
Zhongyu Li <zli@localhost.localdomain>
2016-04-10 23:40:50 -0500

    ohhhhh

--- Gomoku123.py
+++ Gomoku123.py
@@ -1,5 +1,11 @@
 import curses
 
+
+
+actions=['Up','Left','Down','Right','Restart','Exit']
+letter_code=[ord(ch) for ch in 'WASDRQwasdrq'] 
+action_dict=dict(zip(letter_code,action*2))
+
 def main(stdscr):
 	def init():
 	game_board.reset()
@@ -7,20 +13,23 @@ def main(stdscr):
 
 	def not_game(state):
 		
+		action =get_user_action(stdscr)
+				
 	def game();
 
-
+		action = get_user_action(stdscr)
 	
 
 
 
 class GameBoard(object):
 	
-	def __init__(self,width=16,height=16,blackp='X'.whitep='O'):
+	def __init__(self,width=16,height=16,blackp='X',whitep='O',counter=0):
 		self.width=width
 		self.height=height
 		self.blackp=blackp
 		self.whitep=whitep
+		self.counter=counter
 	def boardinit(self):
 		board=[['+' for row in range(17)]for col in range(17)]
 		def setboarder(self):
@@ -44,11 +53,14 @@ class GameBoard(object):
 			cast(board[row] for row in range(16))
 
 		screen.clear()
+		#maybe can cast the counter here
 	def move_is_possible(self):
-	
+		#check if player can spawn the piece on the board
 	def spawn(self):
-		piece=self.blackp if counter%2==1 else self.whitep
+		piece=self.blackp if self.counter%2==1 else self.whitep
 		
 			
-	def winner(self):
-					
+	def is_win(self):
+		
+	def reset(self);
+		 					
