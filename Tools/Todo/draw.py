56308c5a04f9748a8e5b1ad8e036415623d92871
ChenghaoQ <ch.jakobqian@gmail.com>
2016-04-26 11:41:39 -0500

    much more clear

--- Tools/Todo/draw.py
+++ Tools/Todo/draw.py
@@ -1,16 +1,15 @@
 
 
 class Draw(object):
-        def __init__(self,todolist,name):
-                self.todolist=todolist
-        	self.name=name
-        def draw_TODO(self):
-            
-                print("* * * * %s* * * * *"%self.name+'\n')
-                for each in self.todolist:
-                        print('  '.join(each))
-        def draw_string(string):
-                print(string+'\n')
+	def __init__(self,todolist,name):
+		self.todolist=todolist
+		self.name=name
+	def draw_TODO(self):
+		print("* * * * %s* * * * *"%self.name+'\n')
+		for each in self.todolist:
+			print('  '.join(each))
+	def draw_string(string):
+		print(string+'\n')
 
 
 
--- Tools/Todo/yourtodo.py
+++ Tools/Todo/yourtodo.py
@@ -1,25 +1,26 @@
 import useraction
-import todolist
+import checklist
 import datatrans
 import draw
 import os
-import action
+import operation
 def your_todolist():
-	
-	today=todolist.TODO('* * * *Today * * * *')
-       # future=todolist.TODO('* * * *Future * * * *') 
+
+	today=checklist.TODO('* * * *Today * * * *')
 	def init():
 		try:
 			today.tmp='[   ]'
 			today.todolist=datatrans.loadfile()
 		except FileNotFoundError:
 			today.reset('Reset')
-        #                future.reset('Reset')
+#                future.reset('Reset')
 		return 'Todo'
 	def GoToDo():
-                os.system('clear')
-	        draw.Draw(today,today.name)
-                action = useraction.get_user_action()
+		#Draw the list
+		os.system('clear')
+		a=draw.Draw(today,today.name)
+		a.draw_TODO
+		action = useraction.get_user_action()
 
 
 		if action in useraction.moves:
@@ -31,15 +32,17 @@ def your_todolist():
 					action=useraction.get_user_action()
 					continue
 		else:
-			action.operation(today,action)
-			
+			if operation.TODO_operation(today,action)==0:
+				return 'Exit'
+
 		return 'Todo'
-		
+
 	state='Init'
 	state_actions={'Init':init,
-			'Todo':GoToDo}
+	'Todo':GoToDo}
 
 
 	while state !='Exit':
 		state=state_actions[state]()
 
+your_todolist()
