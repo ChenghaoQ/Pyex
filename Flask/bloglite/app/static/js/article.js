a538cec85eb14942706fcb8c6e688e7d99194aab
Chenghao Qian <Jkob@localhost.localdomain>
2016-07-31 16:30:45 -0500

    check

--- Flask/bloglite/app/static/js/article.js
+++ Flask/bloglite/app/static/js/article.js
@@ -1,15 +1,22 @@
 ;$(function()
 {
-    'use strict;
-    var sidebar=$('#sidebar');
+    'use strict';
+    var sidebar=$('#sidebar'),
+        sidebar_trigger = $('#sidebar_trigger');
+    
+    function hideSideBar()
+    {
+        alert('a');
+    }
+    function showSideBar()
+    {
+        alert('a');
+        sidebar.css('left',0);//== sidebar.css('right',0);sidebar.animate({'right':0});
+    }
+    sidebar_trigger.on('click',hideSideBar)
 
 
 
 
 
-
-
-
-
-
-}
+})
