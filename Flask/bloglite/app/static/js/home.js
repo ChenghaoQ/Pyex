3ffca168360bbd932decbcfbc1242538215c1cd1
Zhongyu Li <zli@localhost.localdomain>
2016-08-03 14:38:44 -0500

    sidebar fixed

--- Flask/bloglite/app/static/js/home.js
+++ Flask/bloglite/app/static/js/home.js
@@ -10,11 +10,13 @@
         sidebar.css('left',0);
         /*main_page.css('margin-left',sidebar.width());*/
         main_page.animate({'margin-left':sidebar.width()})
+        sidebar_trigger.text("1");
     }
     function hideSideBar()
     {
         sidebar.css('left',-sidebar.width());
-        main_page.css('margin-left',0);
+        main_page.animate({'margin-left':0})
+        sidebar_trigger.text("2");
     }
     function showhideSideBar()
     {
