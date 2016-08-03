0687878b68314f7e8db03ca120b23b3940d2e6a0
Zhongyu Li <zli@localhost.localdomain>
2016-08-03 00:42:50 -0500

    test

---
+++ Flask/blog/app/static/js/article.js
@@ -0,0 +1,8 @@
+;$(function)({
+    'use strict';
+    var sidebar = $('#sidebar'),
+        sidebar_trigger = $('#sidebar_trigger');
+    
+    sidebar_trigger.on('click',function(){
+        console.log('clicked');
+})
---
+++ Flask/blog/run.py
@@ -0,0 +1,6 @@
+from app import app
+from flask_frozen import Freezer
+freezer = Freezer(app)
+if __name__=='__main__':
+   #     app.run(debug = True,port =9999)
+        freezer.freeze()
