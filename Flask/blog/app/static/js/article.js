e8f68b16cfcf99156dfa1b86d5e9feadbc5a5367
Zhongyu Li <zli@localhost.localdomain>
2016-08-08 13:20:31 -0500

    clean

--- Flask/blog/app/static/js/article.js
+++
@@ -1,8 +0,0 @@
-;$(function)({
-    'use strict';
-    var sidebar = $('#sidebar'),
-        sidebar_trigger = $('#sidebar_trigger');
-    
-    sidebar_trigger.on('click',function(){
-        console.log('clicked');
-})
--- Flask/blog/app/views.py
+++
@@ -1,7 +0,0 @@
-from app import app
-from flask import render_template,url_for
-@app.route('/')
-#@app.route('/index')
-def index():
-        maincss=url_for('static',filename='css/main.css')
-        return render_template('index.html',maincss=maincss)
