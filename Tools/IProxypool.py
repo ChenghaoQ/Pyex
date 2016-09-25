ac9dc7088e85fbe1f5803b9e33ea3e781f946d05
Chenghao Qian <Jkob@localhost.localdomain>
2016-09-25 11:13:53 -0500

    IP pool ad xici

--- Tools/IProxypool.py
+++ Tools/IProxypool.py
@@ -1,30 +1,36 @@
 import re,time,threading,urllib.request
 
 pool = []
-page = 0
+KDLpage = 0
+XCpage = 0
+page = {'KDL':KDLpage,'XCDL':XCpage}
 lock = threading.Lock()
 class ProxySeeker(threading.Thread):
 
-        def __init__(self,thread_id,func):
+        def __init__(self,thread_id,thread_type,func):
                 super(ProxySeeker,self).__init__()
                 self.thread_id = thread_id
                 self.func = func
+                self.thread_type = thread_type 
         def run(self):
                 print("线程 %d 开始工作"%self.thread_id)
-                self.func(self.thread_id)
+                print(self.thread_type)
+                self.func(self.thread_id,self.thread_type)
                 print("线程 %d 完成任务"%self.thread_id)
 
 
 def seeker(parafunc):
-        def in_seeker(thread_id):#decorate need closure to wrap
+        def in_seeker(thread_id,thread_type):#decorate need closure to wrap
                 global pool
                 header = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+                source_name,pages,urls,repattern,thread_id,thread_type = parafunc(thread_id,thread_type)
                 global page
-                source_name,pages,urls,repattern,thread_id = parafunc(thread_id)
-                while page < pages:
+                type(page[thread_type])
+                print("-----------------------------",page[thread_type]) 
+                while page[thread_type] < pages:
                         lock.acquire() #get the lock in case url get page changed by other thread before next line executed
-                        page += 1
-                        page_number = page #copy the page number in case other changes number and cause the Done print with wrong number
+                        page[thread_type] += 1
+                        page_number = page[thread_type] #copy the page number in case other changes number and cause the Done print with wrong number
                         url = eval(urls)
                         lock.release()#release the lock 
                         request = urllib.request.Request(url, headers = header)
@@ -35,19 +41,26 @@ def seeker(parafunc):
                                 prox = prox + (source_name,)
                                 if prox not in pool:
                                         pool.append(prox)
-                        print("Thread %d get page %d Done!"%(thread_id,page_number))
-                        #time.sleep(1)
+                        print("Thread %d get page %d Done! %s"%(thread_id,page_number,source_name))
+                        #time.sleep(1) 
         return in_seeker
 
 
 @seeker
-def kuaidaili(thread_id):
+def kuaidaili(thread_id,thread_type):
         source_name = " |  快代理  "
         pages = 15
-        urls = '"http://www.kuaidaili.com/proxylist/%d/"%page'
-        repattern  = r'<td data-title="IP">(.+?)</td>.*?<td data-title="PORT">(.+?)</td>.*?<td data-title="匿名度">(.+?)</td>.*?<td data-title="类型">(.+?)</td>.*?<td data-title="get/post支持">(.+?)</td>.*?<td data-title="位置">(.+?)</td>.*?<td data-title="响应速度">(.+?)</td>.*?<td data-title="最后验证时间">(.+?)</td>'
-        return source_name,pages,urls,repattern,thread_id
+        urls = '"http://www.kuaidaili.com/proxylist/%d/"%page[thread_type]'
+        repattern  = r'<td data-title="IP">(.+?)</td>.*?<td data-title="PORT">(.+?)</td>.*?<td data-title="类型">(.+?)</td>'
+        return source_name,pages,urls,repattern,thread_id,thread_type
 
+@seeker
+def xici_zh(thread_id,thread_type):
+        source_name = " |  西刺代理"
+        pages = 15
+        urls = '"http://www.xicidaili.com/nn/%d"%page[thread_type]'
+        repattern = r'<tr class=".*?<td>(.+?)</td>.*?<td>(.+?)</td>.*?<td>.*?</td>.*?<td class.*?<td>(.+?)</td>'
+        return source_name,pages,urls,repattern,thread_id,thread_type
 
 
 
@@ -55,13 +68,17 @@ def Proxypool():
         global pool
         page = 1
         print("主线程开始...")
-        threads = [ProxySeeker(i,kuaidaili) for i in range(1,6)]
+        thread_info = [[i,'XCDL',xici_zh] if i > 2 else [i,'KDL',kuaidaili] for i in range(1,5)]
+        threads = [ProxySeeker(t_i[0],t_i[1],t_i[2]) for t_i in thread_info]
         for thread in threads:
                 #time.sleep(0.5)
                 thread.start()
+        for t in threads:
+                t.join()
+        for each in pool:
+                print(each) 
         print("主线程结束...")
-        
-Proxypool()
+        return pool
 
 
 
